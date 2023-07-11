791d9be41e8087b925ba733fade48e295cb92724
DeepaChavadi <90234635+DeepaChavadi@users.noreply.github.com>
2023-07-11 10:12:24 +0530

    Updated

---
+++ Swapping.java
@@ -0,0 +1,18 @@
+package JavaProgramming;
+
+public class Swapping {
+
+	public static void main(String[] args) {
+		int a=6;
+		int b=4;
+		int temp;
+		
+		temp= a;
+		a= b;
+		b=temp;
+		
+		System.out.println("a value " +a );
+		System.out.println("b value " +b);
+	}
+
+}
